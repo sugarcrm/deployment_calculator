5b2962bd876d7719e046a8cbddd9d13742d8ee9c
S. Zachariah Sprackett <zac@sprackett.com>
2017-09-19 15:13:14 -0400

    initial release

---
+++ data.js
@@ -0,0 +1,99 @@
+var data = {
+  "questions": [
+  {
+	  "question": "Do I need access to the latest features on a quarterly basis as they are released?",
+	  "ondemand": {
+		  "y": "success",
+		  "n": "success"
+	  },
+	  "premium": {
+		  "y": "success",
+		  "n": "success"
+	  },
+	  "partner": {
+		  "y": "danger",
+		  "n": "danger"
+	  },
+	  "onprem": {
+		  "y": "danger",
+		  "n": "danger"
+	  }
+  },
+  {
+	  "question": "Do I want control over when new features are rolled out to my Sugar instance?",
+	  "ondemand": {
+		  "y": "danger",
+		  "n": "success"
+	  },
+	  "premium": {
+		  "y": "success",
+		  "n": "success"
+	  },
+	  "partner": {
+		  "y": "success",
+		  "n": "success"
+	  },
+	  "onprem": {
+		  "y": "success",
+		  "n": "success"
+	  }
+  },
+  {
+	  "question": "Do I want direct control over my backup strategy?",
+	  "ondemand": {
+		  "y": "warning",
+		  "n": "success"
+	  },
+	  "premium": {
+		  "y": "warning",
+		  "n": "success"
+	  },
+	  "partner": {
+		  "y": "warning",
+		  "n": "success"
+	  },
+	  "onprem": {
+		  "y": "success",
+		  "n": "success"
+	  }
+  },
+  {
+	  "question": "Do I want to purchase, configure, manage and maintain my own infrastructure?",
+	  "ondemand": {
+		  "y": "danger",
+		  "n": "success"
+	  },
+	  "premium": {
+		  "y": "danger",
+		  "n": "success"
+	  },
+	  "partner": {
+		  "y": "warning",
+		  "n": "success"
+	  },
+	  "onprem": {
+		  "y": "success",
+		  "n": "danger"
+	  }
+  },
+  {
+	  "question": "Do I require upgrades to be perform at a time of my choosing?",
+	  "ondemand": {
+		  "y": "warning",
+		  "n": "success"
+	  },
+	  "premium": {
+		  "y": "success",
+		  "n": "success"
+	  },
+	  "partner": {
+		  "y": "success",
+		  "n": "success"
+	  },
+	  "onprem": {
+		  "y": "success",
+		  "n": "success"
+	  }
+  }
+  ]
+}
