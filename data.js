231dbed2cfc4c6675c2d8b7d1cb4ebfc3128b5a7
S. Zachariah Sprackett <zac@sprackett.com>
2017-09-19 23:17:39 -0400

    update options

--- data.js
+++ data.js
@@ -39,9 +39,28 @@ var data = {
 	  }
   },
   {
-	  "question": "Do I want direct control over my backup strategy?",
+    "question": "Do I require upgrades to be performed at a time of my choosing?",
+    "ondemand": {
+      "y": "warning",
+      "n": "success"
+    },
+    "premium": {
+      "y": "success",
+      "n": "success"
+    },
+    "partner": {
+      "y": "success",
+      "n": "success"
+    },
+    "onprem": {
+      "y": "success",
+      "n": "success"
+    }
+  },
+  {
+	  "question": "Do I want direct control over and responsibility for my own backup strategy?",
 	  "ondemand": {
-		  "y": "warning",
+		  "y": "danger",
 		  "n": "success"
 	  },
 	  "premium": {
@@ -77,23 +96,289 @@ var data = {
 	  }
   },
   {
-	  "question": "Do I require upgrades to be perform at a time of my choosing?",
-	  "ondemand": {
-		  "y": "warning",
-		  "n": "success"
-	  },
-	  "premium": {
-		  "y": "success",
-		  "n": "success"
-	  },
-	  "partner": {
-		  "y": "success",
-		  "n": "success"
-	  },
-	  "onprem": {
-		  "y": "success",
-		  "n": "success"
-	  }
+    "question": "Do I want to be directly responsible for OS level patching?",
+    "ondemand": {
+      "y": "warning",
+      "n": "success"
+    },
+    "premium": {
+      "y": "warning",
+      "n": "success"
+    },
+    "partner": {
+      "y": "warning",
+      "n": "success"
+    },
+    "onprem": {
+      "y": "success",
+      "n": "danger"
+    }
+  },
+  {
+    "question": "Do I have the skills required for keeping my environment secure?",
+    "ondemand": {
+      "y": "success",
+      "n": "success"
+    },
+    "premium": {
+      "y": "success",
+      "n": "success"
+    },
+    "partner": {
+      "y": "success",
+      "n": "success"
+    },
+    "onprem": {
+      "y": "success",
+      "n": "danger"
+    }
+  },
+  {
+    "question": "Am I comfortable managing the unix hosting environment by myself?",
+    "ondemand": {
+      "y": "success",
+      "n": "success"
+    },
+    "premium": {
+      "y": "success",
+      "n": "success"
+    },
+    "partner": {
+      "y": "success",
+      "n": "success"
+    },
+    "onprem": {
+      "y": "success",
+      "n": "danger"
+    }
+  },
+  {
+    "question": "Do I have a database administrator on staff?",
+    "ondemand": {
+      "y": "success",
+      "n": "success"
+    },
+    "premium": {
+      "y": "success",
+      "n": "success"
+    },
+    "partner": {
+      "y": "success",
+      "n": "warning"
+    },
+    "onprem": {
+      "y": "success",
+      "n": "danger"
+    }
+  },
+  {
+    "question": "Do I require my Sugar deployment have a disaster recovery plan in place?",
+    "ondemand": {
+      "y": "success",
+      "n": "success"
+    },
+    "premium": {
+      "y": "success",
+      "n": "success"
+    },
+    "partner": {
+      "y": "warning",
+      "n": "success"
+    },
+    "onprem": {
+      "y": "warning",
+      "n": "success"
+    }
+  },
+  {
+    "question": "Do I want responsibility for creating, testing and executing my own disaster recovery plan?",
+    "ondemand": {
+      "y": "warning",
+      "n": "success"
+    },
+    "premium": {
+      "y": "warning",
+      "n": "success"
+    },
+    "partner": {
+      "y": "warning",
+      "n": "success"
+    },
+    "onprem": {
+      "y": "success",
+      "n": "danger"
+    }
+  },
+  {
+    "question": "Do I have requirements under the GDPR?",
+    "ondemand": {
+      "y": "success",
+      "n": "success"
+    },
+    "premium": {
+      "y": "success",
+      "n": "success"
+    },
+    "partner": {
+      "y": "warning",
+      "n": "success"
+    },
+    "onprem": {
+      "y": "warning",
+      "n": "success"
+    }
+  },
+  {
+    "question": "Do I have specific geographic requirements for the hosting of my CRM data?",
+    "ondemand": {
+      "y": "success",
+      "n": "success"
+    },
+    "premium": {
+      "y": "success",
+      "n": "success"
+    },
+    "partner": {
+      "y": "warning",
+      "n": "success"
+    },
+    "onprem": {
+      "y": "success",
+      "n": "success"
+    }
+  },
+  {
+    "question": "Do I require hosting in a region other than Australia, Ireland or USA?",
+    "ondemand": {
+      "y": "danger",
+      "n": "success"
+    },
+    "premium": {
+      "y": "success",
+      "n": "success"
+    },
+    "partner": {
+      "y": "warning",
+      "n": "success"
+    },
+    "onprem": {
+      "y": "warning",
+      "n": "success"
+    }
+  },
+  {
+    "question": "Do I require PCI, HIPAA or FedRamp compliant hosting?",
+    "ondemand": {
+      "y": "danger",
+      "n": "success"
+    },
+    "premium": {
+      "y": "danger",
+      "n": "success"
+    },
+    "partner": {
+      "y": "warning",
+      "n": "success"
+    },
+    "onprem": {
+      "y": "success",
+      "n": "success"
+    }
+  },
+  {
+    "question": "Do I require that my hosting environment has undergone third party security scans?",
+    "ondemand": {
+      "y": "success",
+      "n": "success"
+    },
+    "premium": {
+      "y": "success",
+      "n": "success"
+    },
+    "partner": {
+      "y": "warning",
+      "n": "success"
+    },
+    "onprem": {
+      "y": "warning",
+      "n": "success"
+    }
+  },
+  {
+    "question": "Do I require that Sugar is only be reachable from behind a VPN connection?",
+    "ondemand": {
+      "y": "danger",
+      "n": "success"
+    },
+    "premium": {
+      "y": "danger",
+      "n": "success"
+    },
+    "partner": {
+      "y": "warning",
+      "n": "success"
+    },
+    "onprem": {
+      "y": "success",
+      "n": "success"
+    }
+  },
+  {
+    "question": "Are all services that Sugar must connect to available via APIs accessible over the internet?",
+    "ondemand": {
+      "y": "danger",
+      "n": "success"
+    },
+    "premium": {
+      "y": "danger",
+      "n": "success"
+    },
+    "partner": {
+      "y": "warning",
+      "n": "success"
+    },
+    "onprem": {
+      "y": "success",
+      "n": "success"
+    }
+  },
+  {
+    "question": "Will my CRM traffic be higher than 20 requests per second?",
+    "ondemand": {
+      "y": "danger",
+      "n": "success"
+    },
+    "premium": {
+      "y": "success",
+      "n": "success"
+    },
+    "partner": {
+      "y": "success",
+      "n": "success"
+    },
+    "onprem": {
+      "y": "success",
+      "n": "success"
+    }
+  },
+  {
+    "question": "Do I require more than 60 Gigabytes of data storage?",
+    "ondemand": {
+      "y": "warning",
+      "n": "success"
+    },
+    "premium": {
+      "y": "success",
+      "n": "success"
+    },
+    "partner": {
+      "y": "success",
+      "n": "success"
+    },
+    "onprem": {
+      "y": "success",
+      "n": "success"
+    }
   }
   ]
 }
